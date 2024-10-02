d65cad234c52e08b5be23b33e13191476a709d38
sosmrraziel <lbt_alh@hotmail.fr>
2024-10-02 15:17:43 +0100

    adding RewardValue class

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+package main.java;
 
 public class RewardsConverter {
     public static void main(String[] args) {
@@ -16,5 +17,35 @@ public class RewardsConverter {
         System.out.println("converting $" + input_value + " to miles");
         var rewardsValue = new RewardValue(cashValue);
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+    }
+}
+
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.01; // Example conversion rate
+
+    // Constructor that accepts a cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    // Constructor that accepts a value in miles
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            this.cashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+        }
+    }
+
+    // Method to get the cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Method to get the miles value
+    public double getMilesValue() {
+        return milesValue;
     }
 }
